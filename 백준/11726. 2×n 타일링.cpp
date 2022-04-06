81956eaaeb507e4a60527956c384abf74cc8f964
rudalsd <100455116+rudalsd@users.noreply.github.com>
2022-04-06 20:39:54 +0900

    22/04/06

--- 백준/11726. 2×n 타일링.cpp
+++ 백준/11726. 2×n 타일링.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int dp[1010] = { 0 };					//값을 저장할 배열 생성
+	int dp[1010] = { 0 };						//값을 저장할 배열 생성
 	int n;
 
 	cin >> n;
@@ -12,15 +12,9 @@ int main()
 	dp[1] = 1;									//첫번째 배열 1로 저장
 	dp[2] = 2;									//두번째 배열 2로 저장
 
-	if (n < 3) {								//n이 3보다 작을 때는 바로 출력
-		cout << dp[n];
-		return 0;
-	}
-	else {
-		for (int i = 3; i <= n; i++) {
-			dp[i] = dp[i - 1] + dp[i - 2];		//점화식
-			dp[i] %= 10007;						//10007로 나눈 나머지 값 저장
-		}
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];			//점화식
+		dp[i] %= 10007;							//10007로 나눈 나머지 값 저장
 	}
 
 	cout << dp[n];								//n번째 값 출력
