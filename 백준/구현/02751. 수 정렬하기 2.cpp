52201445a20398fc2b221483530ada46d60a39a8
Gyeongmin Kwon <rudalsd@naver.com>
2022-08-08 23:05:38 +0900

    22/08/08

---
+++ 백준/구현/02751. 수 정렬하기 2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int arr[1000001];
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	sort(arr, arr + N);
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
+	}
+}
