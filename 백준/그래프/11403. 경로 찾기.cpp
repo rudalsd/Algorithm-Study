0d89ef2adba452ac6fe95b72d9fda86c38bb5a4c
min <rudalsd@naver.com>
2022-09-01 18:28:47 +0900

    22/09/01

---
+++ 백준/그래프/11403. 경로 찾기.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int N;
+int arr[101][101];
+
+int main()
+{
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			scanf("%d", &arr[i][j]);
+		}
+	}
+
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (arr[i][k] == 1 && arr[k][j] == 1) {
+					arr[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
