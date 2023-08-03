da0b7a13e6fab245637f0b38112bbaf880772f5c
권경민 <100455116+rudalsd@users.noreply.github.com>
2023-08-03 10:40:45 +0900

    23/08/03

---
+++ 백준/그래프/20168. 골목 대장 호석 - 기능성.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+struct node {
+	int cur;
+	int cost;
+	int shame;
+};
+
+int N, M, A, B, C;
+int visited[1001][11];
+vector<pair<int,int>> list[11];
+int ans = 987654321;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M >> A >> B >> C;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= 1000; j++) {
+			visited[j][i] = 987654321;
+		}
+	}
+	
+	for (int i = 0; i < M; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		list[a].push_back({ b,c });
+		list[b].push_back({ a,c });
+	}
+
+	queue<node> q;
+
+	q.push({ A,0,0 });
+
+	while (!q.empty()) {
+		const int cur = q.front().cur;
+		const int cost = q.front().cost;
+		const int shame = q.front().shame;
+		q.pop();
+
+		if (cur == B) {
+			ans = min(ans, shame);
+			continue;
+		}
+
+		if (visited[shame][cur] < cost) continue;
+		visited[shame][cur] = cost;
+
+		for (auto& next : list[cur]) {
+			int Max = max(shame, next.second);
+			if (visited[Max][next.first] > cost + next.second && cost + next.second <= C) {
+				visited[Max][next.first] = cost + next.second;
+				q.push({ next.first,cost + next.second, Max });
+			}
+		}
+	}
+
+	if (ans == 987654321) cout << -1;
+	else cout << ans;
+}
